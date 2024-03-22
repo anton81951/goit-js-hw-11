2d95c090f5dfb6effc6157cf3f25c8e6a3f96e65
anton81951 <145291034+anton81951@users.noreply.github.com>
2024-03-22 23:15:07 +0200

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,38 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import { displayImages } from "./render-functions";
 
-const form = document.querySelector('.form');
-const input = document.querySelector('.input');
-
-form.addEventListener("submit", function(event) {
+document.querySelector(".form").addEventListener("submit", event => {
   event.preventDefault();
+  pixabaySearch(event);
+});
+
+function pixabaySearch(event) {
+  event.preventDefault();
+
+  const form = document.querySelector('.form');
+  const input = document.querySelector('.input');
+  const gallery = document.querySelector('.gallery');
 
   const searchInput = input.value.trim();
 
   if (searchInput !== "") {
     const apiKey = '9233093-942588744ee96c4f575017f3e';
     const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchInput}&image_type=photo&orientation=horizontal&safesearch=true`;
+    
     fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network res not ok');
-        }
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => {
         console.log(data);
+        if (data.hits.length === 0) {
+          iziToast.error({
+            title: 'Error',
+            message: "Sorry, there are no images matching your search query. Please try again!",
+            position: 'topCenter'
+          });
+        } else {
+          displayImages(data.hits);
+        }
       })
       .catch(error => {
         console.error('fetch problem', error);
@@ -28,13 +40,11 @@ form.addEventListener("submit", function(event) {
   } else {
     iziToast.error({
       title: 'Error',
-      message: "Sorry, there are no images matching your search query. Please try again!",
-      position: 'topCenter'
-    });
-    iziToast.error({
-      title: 'Error',
       message: "Empty input!",
       position: 'topCenter'
     });
   }
-});
+}
+
+export { pixabaySearch };
+
--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import { displayImages } from "./js/render-functions";
+import { pixabaySearch } from "./js/pixabay-api";
+
+const searchButton = document.querySelector('.btn');
+const gallery = document.querySelector('.gallery');
+const loader = document.createElement('div');
+
+
+searchButton.addEventListener('click', () => {
+    loader.style.display = 'block';
+    loader.classList.add('loader');
+    pixabaySearch().then(() => {
+        loader.style.display = 'none';
+    }).catch(error => {
+        loader.style.display = 'none';
+        iziToast.error({
+            title: 'Error',
+            message: error.message,
+            position: 'topCenter'
+        });
+    });
+});
