97438de6c542c4dbeb5a4e5ca1ca3c56d5f6b572
anton81951 <145291034+anton81951@users.noreply.github.com>
2024-03-23 21:10:45 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { displayImages } from "./js/render-functions";
 import { pixabaySearch } from "./js/pixabay-api";
 
-const searchButton = document.querySelector('.btn');
 const gallery = document.querySelector('.gallery');
 const loader = document.createElement('div');
 loader.style.display = 'none';
 
 document.body.appendChild(loader);
 
-searchButton.addEventListener('click', () => {
+document.querySelector(".form").addEventListener("submit", () => {
     loader.style.display = 'block';
     gallery.innerHTML = '';
     
